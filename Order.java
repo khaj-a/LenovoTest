9a8cd849d8a648b411441ece085fcd17affe09d8
Khaja <shaikkhajapeerrjp@gmail.com>
2026-07-04 12:04:50 +0530

    added Java Changes

---
+++ Order.java
@@ -0,0 +1,14 @@
+public class SizeDataType
+{
+    public static void main(String []args) 
+    {
+    
+       System.out.println("Size of Byte is :-->"+Byte.SIZE);
+       System.out.println("Size of Character is :-->"+Character.SIZE);
+       System.out.println("Size of short is :-->"+Short.SIZE);
+       System.out.println("Size of Integer is :-->"+Integer.SIZE);
+       System.out.println("Size of Long is :-->"+Long.SIZE);
+       System.out.println("Size of Float is :-->"+Float.SIZE);
+       System.out.println("Size of Double is :-->"+Double.SIZE);
+    }
+}
